ef082b522ce17be3a9e7c35d511c97929fc34f07
qwang1990 <wangqi2179@163.com>
2016-05-29 23:30:24 +0800

    init

---
+++ app.js
@@ -0,0 +1,53 @@
+var express = require('express');
+var handlerbar = require('express3-handlebars').create({defaultLayout:'main'});
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.engine('handlebars', handlerbar.engine);
+app.set('view engine', 'handlebars');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(express.static((__dirname, 'public')));
+
+app.use('/', routes);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+
+app.listen(3000,function () {
+    console.log("listen for 3000");
+})
